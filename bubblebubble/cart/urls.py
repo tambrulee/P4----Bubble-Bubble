595e8ce98853b94bf0993044a074c56974166e27
tambrulee <thamiltontestrote@gmail.com>
2025-11-24 22:42:34 +0000

    Cart urls

--- bubblebubble/cart/urls.py
+++ bubblebubble/cart/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'cart'
-urlpatterns = [ path('', views.view_cart, name='view') ]
+app_name = "cart"
+
+urlpatterns = [
+    path("", views.view_cart, name="view"),
+    path("add/<int:product_id>/", views.add_to_cart, name="add"),
+    path("update/<int:item_id>/", views.update_item, name="update"),
+    path("remove/<int:item_id>/", views.remove_item, name="remove"),
+]
