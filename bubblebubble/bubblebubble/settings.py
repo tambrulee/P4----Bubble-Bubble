638921971f933f6f81090718b9332f70a2f7e45a
tambrulee <thamiltontestrote@gmail.com>
2025-12-15 21:24:24 +0000

    User email confirmation

--- bubblebubble/bubblebubble/settings.py
+++ bubblebubble/bubblebubble/settings.py
@@ -130,10 +130,19 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Email settings
-DEFAULT_FROM_EMAIL = "noreply@bubblebubble.shop"
 
-# For development: print emails to console
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = os.getenv(
+    "EMAIL_BACKEND",
+    "django.core.mail.backends.console.EmailBackend",  
+)
+
+EMAIL_HOST = os.getenv("EMAIL_HOST", "")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@bubblebubble.shop")
+
 
 
 # Internationalization
