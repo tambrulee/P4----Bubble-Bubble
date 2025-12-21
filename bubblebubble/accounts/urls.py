1257c4ae75e4e7e8c3b8ec6ece0f7d040ffc7330
tambrulee <thamiltontestrote@gmail.com>
2025-12-21 22:07:52 +0000

    Fix: logout

--- bubblebubble/accounts/urls.py
+++ bubblebubble/accounts/urls.py
@@ -7,6 +7,7 @@ app_name = "accounts"
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="catalog:product_list"), name="logout"),
     path(
         "password-reset/",
         auth_views.PasswordResetView.as_view(
