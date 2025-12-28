4801e1afb980e3c035224744af723fc88705f2c9
tambrulee <thamiltontestrote@gmail.com>
2025-12-28 09:47:04 +0000

    Heroku: Update settings

--- bubblebubble/bubblebubble/settings.py
+++ bubblebubble/bubblebubble/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv 
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,7 +46,7 @@ SECRET_KEY = 'django-insecure-xjlgbpis#n!qaxg6o0lq1)p3pw^cyv5d4ibu*%yb8#2xe%rz7a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
 
 LOGGING = {
     "version": 1,
