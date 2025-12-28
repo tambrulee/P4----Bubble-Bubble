aeeaef3299f0d2fc6c5263c84836ac8fc16437fe
tambrulee <thamiltontestrote@gmail.com>
2025-12-28 14:37:35 +0000

    git env changes

--- bubblebubble/bubblebubble/settings.py
+++ bubblebubble/bubblebubble/settings.py
@@ -45,7 +45,6 @@ STRIPE_CURRENCY = "gbp"
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = config(
